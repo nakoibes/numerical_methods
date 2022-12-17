9ca42221685f9bedb2af545dbab8feb0a9fc6cce
nakoibes <alefanow.d@yandex.ru>
2022-12-17 17:57:45 +0300

    task1 final amends

--- task1/main.cpp
+++ task1/main.cpp
@@ -19,10 +19,10 @@ void f_inter_args(double *args, int length, double a, double b, double delta) {
     int i = 1;
     while (i < length - 1) {
         double value = random(a, b);
-        int good_value = 1;
+        bool good_value = true;
         for (int j = 0; j < i; j++) {
             if (abs(args[j] - value) < delta) {
-                good_value = 0;
+                good_value = false;
                 break;
             }
         }
@@ -101,78 +101,7 @@ void write_f(double *res_args, double *res_vals, double *inter_args, double *int
     fout.close();
 }
 
-//void test_inter(double *interpolation_values, double *interpolation_arguments, int interpolation_length, double arg) {
-//    double value = 0;
-//    double multipliers[interpolation_length];
-//    for (int i = 0; i < interpolation_length; i++) {
-//        double product = 1;
-//        for (int j = 0; j < interpolation_length; j++) {
-//            if (j != i) {
-//                product = product * (interpolation_arguments[i] - interpolation_arguments[j]);
-//            }
-//        }
-//        multipliers[i] = interpolation_values[i] / product;
-//    }
-//
-//    for (int k = 0; k < interpolation_length; k++) {
-//        double product = 1;
-//        for (int j = 0; j < interpolation_length; j++) {
-//            if (k != j) {
-//                product = product * (arg - interpolation_arguments[j]);
-//            }
-//        }
-//        value += multipliers[k] * product;
-//    }
-//    cout << value << endl;
-//}
-
-//void qsort(double *args, double *vals, int size) {
-//    int i = 0;
-//    int j = size - 1;
-//    double mid = args[size / 2];
-//
-//    do {
-//        while (args[i] < mid) {
-//            i++;
-//        }
-//
-//        while (args[j] > mid) {
-//            j--;
-//        }
-//        if (i <= j) {
-//            double tmp = args[i];
-//            args[i] = args[j];
-//            args[j] = tmp;
-//
-//            tmp = vals[i];
-//            vals[i] = vals[j];
-//            vals[j] = tmp;
-//
-//            i++;
-//            j--;
-//        }
-//    } while (i <= j);
-//    if (j > 0) {
-//        qsort(args, vals, j + 1);
-//    }
-//    if (i < size) {
-//        qsort(&args[i], &vals[i], size - i);
-//    }
-//}
-
-//void fill_res(double *res_args, double *res_vals, double *int_args, double *int_vals, double *repr_args, double *repr_vals,
-//         int n_inter, int n_repr) {
-//    for (int i = 0; i < n_inter; i++) {
-//        res_args[i] = int_args[i];
-//        res_vals[i] = int_vals[i];
-//    }
-//    for (int i = 1; i < n_repr - 1; i++) {
-//        res_args[i + n_inter - 1] = repr_args[i];
-//        res_vals[i + n_inter - 1] = repr_vals[i];
-//    }
-//}
-
-void fill_func(double *func, function<double(double)> y, double *res_args, int n_inter, int n_repr) {
+void fill_func(double *func, function<double(double)> y, double *res_args, int n_repr) {
     for (int i = 0; i < n_repr; i++) {
         func[i] = y(res_args[i]);
     }
@@ -183,19 +112,16 @@ int main() {
 
     function<double(double)> y = func;
 
-    double a = -3;
-    double b = 3;
+    double a = -5;
+    double b = 5;
     double delta = 0.0001;
-    int n_inter = 5;
+    int n_inter = 15;
     int n_repr = 2000;
 
     double inter_args[n_inter];
     double inter_vals[n_inter];
     double repr_args[n_repr];
     double repr_vals[n_repr];
-
-//    double res_args[n_inter + n_repr - 2];
-//    double res_vals[n_inter + n_repr - 2];
 
     double func[n_repr];
 
@@ -204,10 +130,7 @@ int main() {
     f_repr_args(repr_args, n_repr, a, b);
     f_repr_vals(repr_vals, repr_args, inter_vals, inter_args, n_repr, n_inter);
 
-//    fill_res(res_args, res_vals, inter_args, inter_vals, repr_args, repr_vals, n_inter, n_repr);
-//    qsort(res_args, res_vals, n_inter + n_repr - 2);
-
-    fill_func(func, y, repr_args, n_inter, n_repr);
+    fill_func(func, y, repr_args, n_repr);
 
     write_f(repr_args, repr_vals, inter_args, inter_vals, func, n_inter, n_repr);
     system("python3 repr.py");
