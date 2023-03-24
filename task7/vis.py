b27e0fcd6329ecc7ebb4701fc548f885b90a48ed
nakoibes <alefanow.d@yandex.ru>
2023-03-24 12:53:35 +0300

    vis amends

--- task7/vis.py
+++ task7/vis.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.arange(-2.0, 2.0, 0.01)
-y = np.arange(-1.0, 1.0, 0.01)
-y1 = x**2-1
-x2 = np.tan(y)
-plt.plot(x, y1, 'r', label='f(x, y) = x^2 - y - 1')
-plt.plot(x2, y, 'b', label='g(x, y) = x - tg y')
-plt.plot(1.396, 0.949, 'o')
-plt.annotate('(1.396, 0.949)', (1.396 + 0.1, 0.949 + 0.1))
+
+x = np.arange(-1.9, 2.0, 0.01)
+# y = np.arange(-1.0, 2.0, 0.01)
+
+y1 = x ** 2
+y2 = np.log(x + 2)
+plt.plot(x, y1, 'r', label='x^2')
+plt.plot(x, y2, 'b', label='log(x+2)')
 
 plt.xlabel('x')
 plt.ylabel('y')
@@ -16,14 +16,16 @@ plt.grid()
 plt.legend()
 plt.show()
 
-x = np.arange(-2.0, 2.0, 0.01)
-y = np.arange(-1.0, 1.0, 0.01)
+x_s = np.arange(-2.0, 2.0, 0.01)
+y_s = np.arange(-1.0, 2.0, 0.01)
 
-xg, yg = np.meshgrid(x, y)
+x, y = np.meshgrid(x_s, y_s)
 
-zg = (xg**2 - yg-1)**2 + (xg - np.tan(yg))**2
+z = (y - x ** 2) ** 2 + (x - np.exp(y) + 2) ** 2
 
-plt.contour(xg, yg, zg, levels = 200)
-plt.title("Disperancy level's sum")
+levels = [0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 1, 2, 5, 10, 20]
+
+plt.contourf(x, y, z, levels=levels)
+plt.title("Contour lines")
 plt.colorbar()
 plt.show()
