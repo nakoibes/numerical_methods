8b9e82eb28f162f17aac26ad8232daf89bf072fd
nakoibes <alefanow.d@yandex.ru>
2023-03-31 13:59:34 +0300

    task7 func amends

--- task7/main.cpp
+++ task7/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <functional>
 
 using namespace std;
 
@@ -134,19 +135,18 @@ void newton_c(double *X0, double *X, double eps) {
         y_p = y_c;
         i++;
     }
-
 }
 
 void newton_d(double *X0, double *X, double eps) {
     int i = 1;
-    double h =0.01;
+    double h = 0.01;
     double x_p = X0[0];
     double y_p = X0[1];
     while (true) {
-        double x_c = x_p + (-f1(x_p, y_p) * df2dy_d(x_p, y_p,h) + f2(x_p, y_p) * df1dy_d(x_p, y_p,h)) /
-                           (df1dx_d(x_p, y_p,h) * df2dy_d(x_p, y_p,h) - df1dy_d(x_p, y_p,h) * df2dx_d(x_p, y_p,h));
-        double y_c = y_p + (-df1dx(x_p, y_p) * f2(x_p, y_p) + f1(x_p, y_p) * df2dx_d(x_p, y_p,h)) /
-                           (df1dx_d(x_p, y_p,h) * df2dy_d(x_p, y_p,h) - df1dy_d(x_p, y_p,h) * df2dx_d(x_p, y_p,h));
+        double x_c = x_p + (-f1(x_p, y_p) * df2dy_d(x_p, y_p, h) + f2(x_p, y_p) * df1dy_d(x_p, y_p, h)) /
+                           (df1dx_d(x_p, y_p, h) * df2dy_d(x_p, y_p, h) - df1dy_d(x_p, y_p, h) * df2dx_d(x_p, y_p, h));
+        double y_c = y_p + (-df1dx(x_p, y_p) * f2(x_p, y_p) + f1(x_p, y_p) * df2dx_d(x_p, y_p, h)) /
+                           (df1dx_d(x_p, y_p, h) * df2dy_d(x_p, y_p, h) - df1dy_d(x_p, y_p, h) * df2dx_d(x_p, y_p, h));
 
         if (abs(x_p - x_c) + abs(y_p - y_c) < eps) {
             X[0] = x_c;
@@ -165,10 +165,69 @@ void newton_d(double *X0, double *X, double eps) {
 
 }
 
+void newton_(double *X0, double *X, double eps, bool con, bool dis, string o_str) {
+    int i = 1;
+    double h = 0.01;
+    double x_p = X0[0];
+    double y_p = X0[1];
+    double d1;
+    double d2;
+    double d3;
+    double d4;
+    if (!dis) {
+        d1 = df1dx(x_p, y_p);
+        d2 = df1dy(x_p, y_p);
+        d3 = df2dx(x_p, y_p);
+        d4 = df2dy(x_p, y_p);
+    } else {
+        d1 = df1dx_d(x_p, y_p, h);
+        d2 = df1dy_d(x_p, y_p, h);
+        d3 = df2dx_d(x_p, y_p, h);
+        d4 = df2dy_d(x_p, y_p, h);
+    }
+
+    while (true) {
+        if (!con) {
+            if (!dis) {
+                d1 = df1dx(x_p, y_p);
+                d2 = df1dy(x_p, y_p);
+                d3 = df2dx(x_p, y_p);
+                d4 = df2dy(x_p, y_p);
+            } else {
+                d1 = df1dx_d(x_p, y_p, h);
+                d2 = df1dy_d(x_p, y_p, h);
+                d3 = df2dx_d(x_p, y_p, h);
+                d4 = df2dy_d(x_p, y_p, h);
+            }
+        }
+        double x_c = x_p + (-f1(x_p, y_p) * d4 + f2(x_p, y_p) * d2) /
+                           (d1 * d4 - d2 * d3);
+        double y_c = y_p + (-d1 * f2(x_p, y_p) + f1(x_p, y_p) * d3) /
+                           (d1 * d4 - d2 * d3);
+
+        if (abs(x_p - x_c) + abs(y_p - y_c) < eps) {
+            X[0] = x_c;
+            X[1] = y_c;
+            cout << o_str << " converged for " << i << " steps, start: " << X0[0] << " ; " << X0[1] << endl << endl;
+            break;
+        }
+        if (i == 10000) {
+            cout << "INFINITE LOOP" << endl << endl;
+            break;
+        }
+        x_p = x_c;
+        y_p = y_c;
+        i++;
+
+    }
+}
+
+
 int main() {
-    double eps = 0.0001;
+    double eps = 0.000001;
     double X01[2] = {-0.5, 0.3};
     double X[2];
+
     simple(X01, X, eps, true);
     cout << X[0] << endl << X[1] << endl;
     X01[0] = 1.1;
@@ -178,29 +237,29 @@ int main() {
 
     X01[0] = -0.5;
     X01[1] = 0.3;
-    newton(X01, X, eps);
+    newton_(X01, X, eps, false, false, "newton");
     cout << X[0] << endl << X[1] << endl;
     X01[0] = 1.1;
     X01[1] = 1.1;
-    newton(X01, X, eps);
+    newton_(X01, X, eps, false, false, "newton");
     cout << X[0] << endl << X[1] << endl;
 
     X01[0] = -0.5;
     X01[1] = 0.3;
-    newton_c(X01, X, eps);
+    newton_(X01, X, eps, true, false, "newton_c");
     cout << X[0] << endl << X[1] << endl;
     X01[0] = 1.1;
     X01[1] = 1.1;
-    newton_c(X01, X, eps);
+    newton_(X01, X, eps, true, false, "newton_c");
     cout << X[0] << endl << X[1] << endl;
 
     X01[0] = -0.5;
     X01[1] = 0.3;
-    newton_d(X01, X, eps);
+    newton_(X01, X, eps, false, true, "newton_d");
     cout << X[0] << endl << X[1] << endl;
     X01[0] = 1.1;
     X01[1] = 1.1;
-    newton_d(X01, X, eps);
+    newton_(X01, X, eps, false, true, "newton_d");
     cout << X[0] << endl << X[1] << endl;
 
     return 0;
