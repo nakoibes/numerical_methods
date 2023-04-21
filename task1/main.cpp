1add1bacd699d7d85329bc6ccf735300ba4651db
nakoibes <alefanow.d@yandex.ru>
2023-04-21 21:17:05 +0300

    task1 naming

--- task1/main.cpp
+++ task1/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-double func(double x) {
+double f(double x) {
     return sin(x);
 }
 
@@ -13,21 +13,21 @@ double random(double min, double max) {
     return (double) (rand()) / RAND_MAX * (max - min) + min;
 }
 
-void f_i_args(double *args, int n, double a, double b, double eps) {
-    args[0] = a;
-    args[n - 1] = b;
+void f_i_x(double *x, int n, double a, double b, double eps) {
+    x[0] = a;
+    x[n - 1] = b;
     int i = 1;
     while (i < n - 1) {
         double value = random(a, b);
         bool good_v = true;
         for (int j = 0; j < i; j++) {
-            if (abs(args[j] - value) < eps) {
+            if (abs(x[j] - value) < eps) {
                 good_v = false;
                 break;
             }
         }
         if (good_v) {
-            args[i] = value;
+            x[i] = value;
             i++;
         }
     }
@@ -49,49 +49,49 @@ void f_even(double *args, int n, double a, double b) {
     }
 }
 
-void f_vis_vals(double *vis_vals, double *vis_args, double *i_vals, double *i_args, int n_v, int n_i) {
+void f_vis_y(double *y_v, double *x_v, double *y_k, double *x_k, int n_v, int n_i) {
     for(int i =0; i < n_v; i++){
-        vis_vals[i] = 0;
+        y_v[i] = 0;
     }
     double mult[n_i];
     for (int i = 0; i < n_i; i++) {
-        double product = 1;
+        double prod = 1;
         for (int j = 0; j < n_i; j++) {
             if (j != i) {
-                product = product * (i_args[i] - i_args[j]);
+                prod *= (x_k[i] - x_k[j]);
             }
         }
-        mult[i] = i_vals[i] / product;
+        mult[i] = y_k[i] / prod;
     }
     for (int i = 0; i < n_v; i++) {
         for (int k = 0; k < n_i; k++) {
             double prod = 1;
             for (int j = 0; j < n_i; j++) {
                 if (k != j) {
-                    prod *= (vis_args[i] - i_args[j]);
+                    prod *= (x_v[i] - x_k[j]);
                 }
             }
-            vis_vals[i] += mult[k] * prod;
+            y_v[i] += mult[k] * prod;
         }
     }
 }
 
-void write_d(double *i_args, double *i_vals, double *i_k_args, double *i_k_vals, double *func, int n_i, int n_v) {
+void write_d(double *x_v, double *y_v, double *x_k, double *y_k, double *func, int n_i, int n_v) {
     ofstream fout("dots.txt");
     for (int i = 0; i < n_v; i++) {
-        fout << i_args[i] << " ";
+        fout << x_v[i] << " ";
     }
     fout << endl;
     for (int i = 0; i < n_v; i++) {
-        fout << i_vals[i] << " ";
+        fout << y_v[i] << " ";
     }
     fout << endl;
     for (int i = 0; i < n_i; i++) {
-        fout << i_k_args[i] << " ";
+        fout << x_k[i] << " ";
     }
     fout << endl;
     for (int i = 0; i < n_i; i++) {
-        fout << i_k_vals[i] << " ";
+        fout << y_k[i] << " ";
     }
     fout << endl;
     for (int i = 0; i < n_v; i++) {
@@ -100,38 +100,37 @@ void write_d(double *i_args, double *i_vals, double *i_k_args, double *i_k_vals,
     fout.close();
 }
 
-void fill_func(double *vals, function<double(double)> y, double *iter, int n) {
+void f_func(double *y, double *x, int n) {
     for (int i = 0; i < n; i++) {
-        vals[i] = y(iter[i]);
+        y[i] = f(x[i]);
     }
 }
 
 int main() {
     srand((unsigned int) time(nullptr));
 
-    function<double(double)> y = func;
 
     double a = -3.14;
     double b = 3.14;
     double eps = 0.0001;
-    int n_i = 20;
-    int n_v = 2000;
+    int n_it = 20;
+    int n_vis = 2000;
 
-    double i_k_args[n_i];
-    double i_k_vals[n_i];
-    double i_args[n_v];
-    double i_vals[n_v];
+    double x_k[n_it];
+    double y_k[n_it];
+    double x_v[n_vis];
+    double y_v[n_vis];
 
-    double func[n_v];
+    double func[n_vis];
 
-    f_i_args(i_k_args, n_i, a, b, eps);
-    fill_func(i_k_vals, y, i_k_args, n_i);
-    f_even(i_args, n_v, a, b);
-    f_vis_vals(i_vals, i_args, i_k_vals, i_k_args, n_v, n_i);
+    f_i_x(x_k, n_it, a, b, eps);
+    f_func(y_k, x_k, n_it);
+    f_even(x_v, n_vis, a, b);
+    f_vis_y(y_v, x_v, y_k, x_k, n_vis, n_it);
 
-    fill_func(func, y, i_args, n_v);
+    f_func(func, x_v, n_vis);
 
-    write_d(i_args, i_vals, i_k_args, i_k_vals, func, n_i, n_v);
+    write_d(x_v, y_v, x_k, y_k, func, n_it, n_vis);
 
     system("python3 vis.py");
     return 0;
