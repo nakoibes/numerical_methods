58695d4ad1a4bf620de625d72a8b231ffbf4aacc
nakoibes <alefanow.d@yandex.ru>
2023-04-22 12:25:56 +0300

    task2 fixes names mostly

--- task2/main.cpp
+++ task2/main.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 
-double func(double x) {
+double f(double x) {
     return sin(x);
 }
 
-void f_inter_args(double **args, int len_1, int len_2, double a, double step) {
+void f_i_k_args(double **args, int len_1, int len_2, double a, double step) {
     double current = a;
     for (int i = 0; i < len_1; i++) {
         for (int j = 0; j < len_2; j++) {
@@ -21,15 +21,15 @@ void f_inter_args(double **args, int len_1, int len_2, double a, double step) {
     }
 }
 
-void f_inter_vals(double **args, double **vals, function<double(double)> y, int len_1, int len_2) {
-    for (int i = 0; i < len_1; i++) {
-        for (int j = 0; j < len_2; j++) {
-            vals[i][j] = y(args[i][j]);
+void f_i_vals(double **X, double **Y, int k, int n1) {
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < n1; j++) {
+            Y[i][j] = f(X[i][j]);
         }
     }
 }
 
-void f_repr_args(double *repr_args, int n_repr, double a, double b) {
+void f_even(double *repr_args, int n_repr, double a, double b) {
     repr_args[0] = a;
     repr_args[n_repr - 1] = b;
     double step = (b - a) / (n_repr - 1);
@@ -38,142 +38,143 @@ void f_repr_args(double *repr_args, int n_repr, double a, double b) {
     }
 }
 
-void interpolate(double *repr_vals, double *repr_args, double *inter_vals, double *inter_args, int n_repr, int n_inter) {
-    for (int i = 0; i < n_repr; i++) {
-        repr_vals[i] = 0;
+void interp(double *y, double *x, double *y_k, double *x_k, int n, int n_k) {
+    for (int i = 0; i < n; i++) {
+        y[i] = 0;
     }
-    double multipliers[n_inter];
-    for (int i = 0; i < n_inter; i++) {
-        double product = 1;
-        for (int j = 0; j < n_inter; j++) {
+    double mult[n_k];
+    for (int i = 0; i < n_k; i++) {
+        double prod = 1;
+        for (int j = 0; j < n_k; j++) {
             if (j != i) {
-                product = product * (inter_args[i] - inter_args[j]);
+                prod *= (x_k[i] - x_k[j]);
             }
         }
-        multipliers[i] = inter_vals[i] / product;
+        mult[i] = y_k[i] / prod;
     }
-    for (int i = 0; i < n_repr; i++) {
-        for (int k = 0; k < n_inter; k++) {
-            double product = 1;
-            for (int j = 0; j < n_inter; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < n_k; k++) {
+            double prod = 1;
+            for (int j = 0; j < n_k; j++) {
                 if (k != j) {
-                    product = product * (repr_args[i] - inter_args[j]);
+                    prod *= (x[i] - x_k[j]);
                 }
             }
-            repr_vals[i] += multipliers[k] * product;
+            y[i] += mult[k] * prod;
         }
     }
 }
 
-void f_repr_vals(double *repr_vals, double *repr_args, double **inter_vals, double **inter_args, int n_repr, int len_1, int len_2) {
+void f_y(double *y, double *x, double **Y, double **X, int k, int n1) {
     double eps = 0.000001;
     int start = 0;
     int end = 0;
-    int len;
-    for (int i = 0; i < len_1; i++) {
-        double current = inter_args[i][len_2 - 1];
-        while (current-repr_args[end] > eps) {
+    int l;
+    for (int i = 0; i < k; i++) {
+        double current = X[i][n1 - 1];
+        while (current - x[end] > eps) {
             end++;
         }
-        len = end - start + 1;
-        interpolate(&repr_vals[start], &repr_args[start], inter_vals[i], inter_args[i], len, len_2);
+        l = end - start + 1;
+        interp(&y[start], &x[start], Y[i], X[i], l, n1);
         start = end;
     }
 }
 
-void f_res_int(double *res_int_args, double *res_int_vals, double **int_args, double **int_vals, int len_1, int len_2) {
-    int k = 0;
-    for (int i = 0; i < len_1; i++) {
-        for (int j = 0; j < len_2; j++) {
-            if (i != len_1 - 1) {
-                if (j != len_2 - 1) {
-                    res_int_args[k] = int_args[i][j];
-                    res_int_vals[k] = int_vals[i][j];
-                    k++;
+
+void f_res_int(double *res_int_args, double *res_int_vals, double **int_args, double **int_vals, int k, int n1) {
+    int l = 0;
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < n1; j++) {
+            if (i != k - 1) {
+                if (j != n1 - 1) {
+                    res_int_args[l] = int_args[i][j];
+                    res_int_vals[l] = int_vals[i][j];
+                    l++;
                 }
             }
             else {
-                res_int_args[k] = int_args[i][j];
-                res_int_vals[k] = int_vals[i][j];
-                k++;
+                res_int_args[l] = int_args[i][j];
+                res_int_vals[l] = int_vals[i][j];
+                l++;
             }
         }
     }
 }
 
-void write_f(double *res_args, double *res_vals, double *inter_args, double *inter_vals, double *func,double* div_knots_args,double* div_knots_vals,
-             int n_inter, int n_res, int k) {
+void write_f(double *x_v, double *y_v, double *x_k, double *y_k, double *func, double* el_k_x, double* el_k_y,
+             int m, int n_v, int k) {
     ofstream fout("dots.txt");
-    for (int i = 0; i < n_res; i++) {
-        fout << res_args[i] << " ";
+    for (int i = 0; i < n_v; i++) {
+        fout << x_v[i] << " ";
     }
     fout << endl;
-    for (int i = 0; i < n_res; i++) {
-        fout << res_vals[i] << " ";
+    for (int i = 0; i < n_v; i++) {
+        fout << y_v[i] << " ";
     }
     fout << endl;
-    for (int i = 0; i < n_inter; i++) {
-        fout << inter_args[i] << " ";
+    for (int i = 0; i < m; i++) {
+        fout << x_k[i] << " ";
     }
     fout << endl;
-    for (int i = 0; i < n_inter; i++) {
-        fout << inter_vals[i] << " ";
-    }
-    fout << endl;
-    for (int i = 0; i < k+1; i++) {
-        fout << div_knots_args[i] << " ";
+    for (int i = 0; i < m; i++) {
+        fout << y_k[i] << " ";
     }
     fout << endl;
     for (int i = 0; i < k+1; i++) {
-        fout << div_knots_vals[i] << " ";
+        fout << el_k_x[i] << " ";
     }
     fout << endl;
-    for (int i = 0; i < n_res; i++) {
+    for (int i = 0; i < k+1; i++) {
+        fout << el_k_y[i] << " ";
+    }
+    fout << endl;
+    for (int i = 0; i < n_v; i++) {
         fout << func[i] << " ";
     }
     fout.close();
 }
 
-void fill_func(double *func, function<double(double)> y, double *res_args, int n_repr) {
-    for (int i = 0; i < n_repr; i++) {
-        func[i] = y(res_args[i]);
+void f_func(double *y, double *x, int n) {
+    for (int i = 0; i < n; i++) {
+        y[i] = f(x[i]);
     }
 }
 
-double calc_abs_err_1(double *func, double *inter, int n_err) {
+double calc_abs_err_1(double *v1, double *v2, int n) {
     double result = 0;
-    for (int i = 0; i < n_err; i++) {
-        result += abs(func[i] - inter[i]);
+    for (int i = 0; i < n; i++) {
+        result += abs(v1[i] - v2[i]);
     }
     return result;
 }
 
-double calc_abs_err_2(double *func, double *inter, int n_err) {
+double calc_abs_err_2(double *v1, double *v2, int n) {
     double result = 0;
-    for (int i = 0; i < n_err; i++) {
-        result += pow(abs(func[i] - inter[i]), 2);
+    for (int i = 0; i < n; i++) {
+        result += pow(abs(v1[i] - v2[i]), 2);
     }
     return sqrt(result);
 }
 
-double calc_abs_err_cheb(double *func, double *inter, int n_err) {
+double calc_abs_err_cheb(double *v1, double *v2, int n) {
     double result = 0;
-    for (int i = 0; i < n_err; i++) {
-        if (abs(func[i] - inter[i]) > result) {
-            result = abs(func[i] - inter[i]);
+    for (int i = 0; i < n; i++) {
+        if (abs(v1[i] - v2[i]) > result) {
+            result = abs(v1[i] - v2[i]);
         }
     }
     return result;
 }
 
-double calc_rel_err_1(double *func, double *inter, int n_err) {
+double calc_rel_err_1(double *v1, double *v2, int n) {
     double num = 0;
     double den = 0;
-    for (int i = 0; i < n_err; i++) {
-        num += (abs(func[i] - inter[i]));
+    for (int i = 0; i < n; i++) {
+        num += (abs(v1[i] - v2[i]));
     }
-    for (int i = 0; i < n_err; i++) {
-        den += abs(func[i]);
+    for (int i = 0; i < n; i++) {
+        den += abs(v1[i]);
     }
     if(den == 0.0){
         den = 1;
@@ -181,14 +182,14 @@ double calc_rel_err_1(double *func, double *inter, int n_err) {
     return num / den;
 }
 
-double calc_rel_err_2(double *func, double *inter, int n_err) {
+double calc_rel_err_2(double *v1, double *v2, int n) {
     double num = 0;
     double den = 0;
-    for (int i = 0; i < n_err; i++) {
-        num += (pow(abs(func[i] - inter[i]), 2));
+    for (int i = 0; i < n; i++) {
+        num += (pow(abs(v1[i] - v2[i]), 2));
     }
-    for (int i = 0; i < n_err; i++) {
-        den += pow(func[i], 2);
+    for (int i = 0; i < n; i++) {
+        den += pow(v1[i], 2);
     }
     if(den == 0.0){
         den = 1;
@@ -196,17 +197,17 @@ double calc_rel_err_2(double *func, double *inter, int n_err) {
     return sqrt(num)/sqrt(den);
 }
 
-double calc_rel_err_cheb(double *func, double *inter, int n_err) {
+double calc_rel_err_cheb(double *v1, double *v2, int n) {
     double num = 0;
     double den = 0;
-    for (int i = 0; i < n_err; i++) {
-        if (abs(func[i] - inter[i]) > num) {
-            num = (abs(func[i] - inter[i]));
+    for (int i = 0; i < n; i++) {
+        if (abs(v1[i] - v2[i]) > num) {
+            num = (abs(v1[i] - v2[i]));
         }
     }
-    for (int i = 0; i < n_err; i++) {
-        if (abs(func[i]) > den) {
-            den = (abs(func[i]));
+    for (int i = 0; i < n; i++) {
+        if (abs(v1[i]) > den) {
+            den = (abs(v1[i]));
         }
     }
     if(den == 0.0){
@@ -215,99 +216,90 @@ double calc_rel_err_cheb(double *func, double *inter, int n_err) {
     return num / den;
 }
 
-void write_errs(double* err_func,double* err_vals,int n_err) {
+void write_errs(double* e_f, double* e_y, int n) {
     ofstream fout("errs.txt");
     fout << fixed;
     fout.precision(6);
-    fout << "abs_err_1 " << scientific << calc_abs_err_1(err_func, err_vals, n_err) << endl;
-    fout << "abs_err_2 " << calc_abs_err_2(err_func, err_vals, n_err) << endl;
-    fout << "abs_err_c " << calc_abs_err_cheb(err_func, err_vals, n_err) << endl;
-    fout << "rel_err_1 " << calc_rel_err_1(err_func, err_vals, n_err) << endl;
-    fout << "rel_err_2 " << calc_rel_err_2(err_func, err_vals, n_err) << endl;
-    fout << "rel_err_c " << calc_rel_err_cheb(err_func, err_vals, n_err) << endl;
+    fout << "abs_err_1   " << scientific << calc_abs_err_1(e_f, e_y, n) << endl;
+    fout << "abs_err_2   " << calc_abs_err_2(e_f, e_y, n) << endl;
+    fout << "abs_err_c   " << calc_abs_err_cheb(e_f, e_y, n) << endl;
+    fout << "rel_err_1   " << calc_rel_err_1(e_f, e_y, n) << endl;
+    fout << "rel_err_2   " << calc_rel_err_2(e_f, e_y, n) << endl;
+    fout << "rel_err_c   " << calc_rel_err_cheb(e_f, e_y, n) << endl;
     fout.close();
 }
 
-void f_div_knots_args(double* div_knots,double** args,int k,int n){
-    div_knots[0] = args[0][0];
+void f_k(double* knots, double** data, int k, int n){
+    knots[0] = data[0][0];
     for(int i = 0;i<k;i++){
-        div_knots[i+1] = args[i][n];
+        knots[i + 1] = data[i][n];
     }
 }
-void f_div_knots_vals(double* div_knots,double** vals,int k,int n){
-    div_knots[0] = vals[0][0];
-    for(int i = 0;i<k;i++){
-        div_knots[i+1] = vals[i][n];
-    }
-}
-
 int main() {
-    function<double(double)> y = func;
+    double a =-3.14;
+    double b = 3.14;
 
-    double a = 0.0;
-    double b = 8.0;
-
-    int k = 2;
-    int n = 5;//степень
-    int n_repr = 2000;
+    int k = 4;
+    int n = 2;  //степень
+    int n_v = 2000;
 
     int m = n * k + 1;
 
-    double int_len = (b - a) / k;
-    double h = int_len / n;
+    double int_l = (b - a) / k;
+    double h = int_l / n;
 
-    int n_err = 100*m-99;//right
+    int n_err = 100*m-99;
 
-    double **int_args = new double *[k];
+    double **X_k = new double *[k];
     for (int i = 0; i < k; i++) {
-        int_args[i] = new double[n + 1];
+        X_k[i] = new double[n + 1];
     }
 
-    double **int_vals = new double *[k];
+    double **Y_k = new double *[k];
     for (int i = 0; i < k; i++) {
-        int_vals[i] = new double[n + 1];
+        Y_k[i] = new double[n + 1];
     }
 
-    double res_int_args[m];
-    double res_int_vals[m];
-    double repr_args[n_repr];
-    double repr_vals[n_repr];
-    double func[n_repr];
-    double err_func[n_err];
-    double err_args[n_err];
-    double err_vals[n_err];
+    double x_k[m];
+    double y_k[m];
+    double x_v[n_v];
+    double y_v[n_v];
+    double func[n_v];
+    double func100[n_err];
+    double x100[n_err];
+    double y100[n_err];
 
-    double div_knots_args[k+1];
-    double div_knots_vals[k+1];
+    double el_k_x[k + 1];
+    double el_k_y[k + 1];
 
-    f_inter_args(int_args, k, n + 1, a, h);
-    f_div_knots_args(div_knots_args, int_args,k,n);
-    f_inter_vals(int_args, int_vals, y, k, n + 1);
-    f_div_knots_vals(div_knots_vals, int_vals,k,n);
-    f_repr_args(repr_args, n_repr, a, b);
-    f_repr_vals(repr_vals, repr_args, int_vals, int_args, n_repr, k, n + 1);
-    f_res_int(res_int_args, res_int_vals, int_args, int_vals, k, n + 1);
+    f_i_k_args(X_k, k, n + 1, a, h);
+    f_k(el_k_x, X_k, k, n);
+    f_i_vals(X_k, Y_k, k, n + 1);
+    f_k(el_k_y, Y_k, k, n);
+    f_even(x_v, n_v, a, b);
+    f_y(y_v, x_v, Y_k, X_k, k, n + 1);
+    f_res_int(x_k, y_k, X_k, Y_k, k, n + 1);
 
-    fill_func(func, y, repr_args, n_repr);
+    f_func(func, x_v, n_v);
 
-    f_repr_args(err_args, n_err, a, b);
-    fill_func(err_func, y, err_args, n_err);
-    f_repr_vals(err_vals, err_args, int_vals, int_args, n_err, k, n + 1);
+    f_even(x100, n_err, a, b);
+    f_func(func100, x100, n_err);
+    f_y(y100, x100, Y_k, X_k, k, n + 1);
 
-    write_errs(err_func,err_vals,n_err);
-    write_f(repr_args, repr_vals, res_int_args, res_int_vals, func,div_knots_args,div_knots_vals, m, n_repr,k);
+    write_errs(func100, y100, n_err);
+    write_f(x_v, y_v, x_k, y_k, func, el_k_x, el_k_y, m, n_v, k);
 
-    system("python3 repr.py");
-
-    for (int i = 0; i < k; i++) {
-        delete[] int_args[i];
-    }
-    delete[] int_args;
+    system("python3 vis.py");
 
     for (int i = 0; i < k; i++) {
-        delete[] int_vals[i];
+        delete[] X_k[i];
     }
-    delete[] int_vals;
+    delete[] X_k;
+
+    for (int i = 0; i < k; i++) {
+        delete[] Y_k[i];
+    }
+    delete[] Y_k;
 
     return 0;
 }
