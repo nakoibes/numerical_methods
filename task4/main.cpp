999e26bdce1c5d1d85aca6ccdbb571a5e50f30c7
nakoibes <alefanow.d@yandex.ru>
2023-04-22 13:36:52 +0300

    task4 names

--- task4/main.cpp
+++ task4/main.cpp
@@ -1,139 +1,141 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
-#include <functional>
 #include <iomanip>
 
 using namespace std;
 
-double func(double x) {
-    return x*x*x;
+double f(double x) {
+    return sin(x);
 }
 
-double d_func(double x) {
-    return 3*x*x;
+double df(double x) {
+    return cos(x);
 }
 
-void f_even_args(double *args, int n, double a, double b) {
-    args[0] = a;
-    args[n - 1] = b;
+void f_even(double *x, int n, double a, double b) {
+    x[0] = a;
+    x[n - 1] = b;
     double step = (b - a) / (n - 1);
     for (int i = 1; i < n - 1; i++) {
-        args[i] = args[i - 1] + step;
+        x[i] = x[i - 1] + step;
     }
 }
 
-void fill_func(double *func, function<double(double)> y, double *args, int n) {
+void fill_func(double *func, double *x, int n) {
     for (int i = 0; i < n; i++) {
-        func[i] = y(args[i]);
+        func[i] = f(x[i]);
     }
 }
 
-void differentiate(double *vals, double *der, int n, double h) {
-    der[0] = (-3 * vals[0] + 4 * vals[1] - vals[2]) / (2 * h);
-    der[n - 1] = (vals[n - 3] - 4 * vals[n - 2] + 3 * vals[n - 1]) / (2 * h);
+void fill_dfunc(double *func, double *x, int n) {
+    for (int i = 0; i < n; i++) {
+        func[i] = df(x[i]);
+    }
+}
+
+void differentiate(double *y, double *dy, int n, double h) {
+    dy[0] = (-3 * y[0] + 4 * y[1] - y[2]) / (2 * h);
+    dy[n - 1] = (y[n - 3] - 4 * y[n - 2] + 3 * y[n - 1]) / (2 * h);
     for (int i = 1; i < n - 1; i++) {
-        der[i] = (vals[i + 1] - vals[i - 1]) / (2 * h);
+        dy[i] = (y[i + 1] - y[i - 1]) / (2 * h);
     }
 }
 
-void calc_main_err(double *main_err, double *der1, double *der2, int m1) {
-    for (int i = 0; i < m1; i+=1) {
-        main_err[i] = (der1[i] - der2[2*i]) / (0.5 * 0.5 - 1);
+void calc_main_err(double *main_err, double *dy1, double *dy2, int m1) {
+    for (int i = 0; i < m1; i += 1) {
+        main_err[i] = (dy1[i] - dy2[2 * i]) / (0.5 * 0.5 - 1.0);
     }
 }
 
-void calc_runge(double *runge, double *main_err, double *der, int n) {
+void calc_runge(double *runge, double *main_err, double *dy, int n) {
     for (int i = 0; i < n; i++) {
-        runge[i] = der[i] + main_err[i];
+        runge[i] = dy[i] + main_err[i];
     }
 }
 
-double calc_abs_err_1(double *func, double *inter, int n_err) {
+double calc_abs_err_1(double *v1, double *v2, int n) {
     double result = 0.0;
-    for (int i = 0; i < n_err; i++) {
-        result += abs(func[i] - inter[i]);
+    for (int i = 0; i < n; i++) {
+        result += abs(v1[i] - v2[i]);
     }
     return result;
 }
 
-double calc_abs_err_2(double *func, double *inter, int n_err) {
+double calc_abs_err_2(double *v1, double *v2, int n) {
     double result = 0.0;
-    for (int i = 0; i < n_err; i++) {
-        result += pow(abs(func[i] - inter[i]), 2);
+    for (int i = 0; i < n; i++) {
+        result += pow(abs(v1[i] - v2[i]), 2);
     }
     return sqrt(result);
 }
 
-double calc_abs_err_cheb(double *func, double *inter, int n_err) {
+double calc_abs_err_cheb(double *v1, double *v2, int n) {
     double result = 0.0;
-    for (int i = 0; i < n_err; i++) {
-        if (abs(func[i] - inter[i]) > result) {
-            result = abs(func[i] - inter[i]);
+    for (int i = 0; i < n; i++) {
+        if (abs(v1[i] - v2[i]) > result) {
+            result = abs(v1[i] - v2[i]);
         }
     }
     return result;
 }
 
-double calc_rel_err_1(double *func, double *inter, int n_err) {
-    double eps = 0.000001;
+double calc_rel_err_1(double *v1, double *v2, int n) {
+    double eps = 0.000000001;
     double num = 0.0;
     double den = 0.0;
-    for (int i = 0; i < n_err; i++) {
-        num += (abs(func[i] - inter[i]));
+    for (int i = 0; i < n; i++) {
+        num += (abs(v1[i] - v2[i]));
     }
-    for (int i = 0; i < n_err; i++) {
-        den += abs(func[i]);
+    for (int i = 0; i < n; i++) {
+        den += abs(v1[i]);
     }
-    if (abs(den - 0.0) < eps) {
+    if (den < eps) {
         den = 1.0;
     }
     return num / den;
 }
 
-double calc_rel_err_2(double *func, double *inter, int n_err) {
-    double eps = 0.000001;
+double calc_rel_err_2(double *v1, double *v2, int n) {
+    double eps = 0.000000001;
     double num = 0.0;
     double den = 0.0;
-    for (int i = 0; i < n_err; i++) {
-        num += (pow(abs(func[i] - inter[i]), 2));
+    for (int i = 0; i < n; i++) {
+        num += (pow(abs(v1[i] - v2[i]), 2));
     }
-    for (int i = 0; i < n_err; i++) {
-        den += pow(func[i], 2);
+    for (int i = 0; i < n; i++) {
+        den += pow(v1[i], 2);
     }
-    if (abs(den - 0.0) < eps) {
+    if (den < eps) {
         den = 1.0;
     }
     return sqrt(num) / sqrt(den);
 }
 
-double calc_rel_err_cheb(double *func, double *inter, int n_err) {
-    double eps = 0.000001;
+double calc_rel_err_cheb(double *v1, double *v2, int n) {
+    double eps = 0.000000001;
     double num = 0.0;
     double den = 0.0;
-    for (int i = 0; i < n_err; i++) {
-        if (abs(func[i] - inter[i]) > num) {
-            num = (abs(func[i] - inter[i]));
+    for (int i = 0; i < n; i++) {
+        if (abs(v1[i] - v2[i]) > num) {
+            num = (abs(v1[i] - v2[i]));
         }
     }
-    for (int i = 0; i < n_err; i++) {
-        if (abs(func[i]) > den) {
-            den = (abs(func[i]));
+    for (int i = 0; i < n; i++) {
+        if (abs(v1[i]) > den) {
+            den = (abs(v1[i]));
         }
     }
-    if (abs(den - 0.0) < eps) {
+    if (den < eps) {
         den = 1.0;
     }
     return num / den;
 }
 
-void write_errs1(double *err_func1, double *err_vals1, int n_err1,
-                 double *err_func2, double *err_vals2, int n_err2,
-                 double *err_vals3,
-                 double* main_err,
-                 string filename) {
-    double zeros[n_err1];
-    for(int i=0;i<n_err1;i++){zeros[i] = 0.0;}
+void write_errs1(double *dy_h, double *der1, int n1, double *dy_h2, double *der2, int n2,
+                 double *runge, double *main_err, string filename) {
+    double zeros[n1];
+    for (int i = 0; i < n1; i++) { zeros[i] = 0.0; }
     ofstream fout(filename);
     fout << fixed;
     fout.precision(6);
@@ -145,133 +147,130 @@ void write_errs1(double *err_func1, double *err_vals1, int n_err1,
          << endl;
 
     fout << setw(15) << left << "abs_err_1"
-         << setw(15) << left << scientific << calc_abs_err_1(err_func1, err_vals1, n_err1)
-         << setw(15) << left << calc_abs_err_1(err_func2, err_vals2, n_err2)
-         << setw(15) << left << calc_abs_err_1(err_func1, err_vals3, n_err1)
-         << setw(15) << left << calc_abs_err_1(zeros, main_err, n_err1)
+         << setw(15) << left << scientific << calc_abs_err_1(dy_h, der1, n1)
+         << setw(15) << left << calc_abs_err_1(dy_h2, der2, n2)
+         << setw(15) << left << calc_abs_err_1(dy_h, runge, n1)
+         << setw(15) << left << calc_abs_err_1(zeros, main_err, n1)
          << endl;
 
     fout << setw(15) << left << "abs_err_2"
-         << setw(15) << left << calc_abs_err_2(err_func1, err_vals1, n_err1)
-         << setw(15) << left << calc_abs_err_2(err_func2, err_vals2, n_err2)
-         << setw(15) << left << calc_abs_err_2(err_func1, err_vals3, n_err1)
-         << setw(15) << left << calc_abs_err_2(zeros, main_err, n_err1)
+         << setw(15) << left << calc_abs_err_2(dy_h, der1, n1)
+         << setw(15) << left << calc_abs_err_2(dy_h2, der2, n2)
+         << setw(15) << left << calc_abs_err_2(dy_h, runge, n1)
+         << setw(15) << left << calc_abs_err_2(zeros, main_err, n1)
          << endl;
 
     fout << setw(15) << left << "abs_err_c"
-         << setw(15) << left << calc_abs_err_cheb(err_func1, err_vals1, n_err1)
-         << setw(15) << left << calc_abs_err_cheb(err_func2, err_vals2, n_err2)
-         << setw(15) << left << calc_abs_err_cheb(err_func1, err_vals3, n_err1)
-         << setw(15) << left << calc_abs_err_cheb(zeros, main_err, n_err1)
+         << setw(15) << left << calc_abs_err_cheb(dy_h, der1, n1)
+         << setw(15) << left << calc_abs_err_cheb(dy_h2, der2, n2)
+         << setw(15) << left << calc_abs_err_cheb(dy_h, runge, n1)
+         << setw(15) << left << calc_abs_err_cheb(zeros, main_err, n1)
          << endl;
 
     fout << setw(15) << left << "rel_err_1"
-         << setw(15) << left << calc_rel_err_1(err_func1, err_vals1, n_err1)
-         << setw(15) << left << calc_rel_err_1(err_func2, err_vals2, n_err2)
-         << setw(15) << left << calc_rel_err_1(err_func1, err_vals3, n_err1)
+         << setw(15) << left << calc_rel_err_1(dy_h, der1, n1)
+         << setw(15) << left << calc_rel_err_1(dy_h2, der2, n2)
+         << setw(15) << left << calc_rel_err_1(dy_h, runge, n1)
          << endl;
 
     fout << setw(15) << left << "rel_err_2"
-         << setw(15) << left << calc_rel_err_2(err_func1, err_vals1, n_err1)
-         << setw(15) << left << calc_rel_err_2(err_func2, err_vals2, n_err2)
-         << setw(15) << left << calc_rel_err_2(err_func1, err_vals3, n_err1)
+         << setw(15) << left << calc_rel_err_2(dy_h, der1, n1)
+         << setw(15) << left << calc_rel_err_2(dy_h2, der2, n2)
+         << setw(15) << left << calc_rel_err_2(dy_h, runge, n1)
          << endl;
 
     fout << setw(15) << left << "rel_err_c"
-         << setw(15) << left << calc_rel_err_cheb(err_func1, err_vals1, n_err1)
-         << setw(15) << left << calc_rel_err_cheb(err_func2, err_vals2, n_err2)
-         << setw(15) << left << calc_rel_err_cheb(err_func1, err_vals3, n_err1)
+         << setw(15) << left << calc_rel_err_cheb(dy_h, der1, n1)
+         << setw(15) << left << calc_rel_err_cheb(dy_h2, der2, n2)
+         << setw(15) << left << calc_rel_err_cheb(dy_h, runge, n1)
          << endl;
 
     fout.close();
 }
 
-void write_f(double *f_args, double *f_vals, double *a1_args, double *a1_vals, double *a2_args, double *a2_vals,
-        double *ar_args, double *ar_vals, int n1, int n2, int n_viz) {
+void write_f(double *x_v, double *y_v, double *x_h, double *der1, double *x_h2, double *der2,
+             double *runge, int n1, int n2, int n_v) {
     ofstream fout("dots.txt");
-    for (int i = 0; i < n_viz; i++) {
-        fout << f_args[i] << " ";
+    for (int i = 0; i < n_v; i++) {
+        fout << x_v[i] << " ";
     }
     fout << endl;
-    for (int i = 0; i < n_viz; i++) {
-        fout << f_vals[i] << " ";
-    }
-    fout << endl;
-    for (int i = 0; i < n1; i++) {
-        fout << a1_args[i] << " ";
+    for (int i = 0; i < n_v; i++) {
+        fout << y_v[i] << " ";
     }
     fout << endl;
     for (int i = 0; i < n1; i++) {
-        fout << a1_vals[i] << " ";
+        fout << x_h[i] << " ";
+    }
+    fout << endl;
+    for (int i = 0; i < n1; i++) {
+        fout << der1[i] << " ";
     }
     fout << endl;
     for (int i = 0; i < n2; i++) {
-        fout << a2_args[i] << " ";
+        fout << x_h2[i] << " ";
     }
     fout << endl;
     for (int i = 0; i < n2; i++) {
-        fout << a2_vals[i] << " ";
+        fout << der2[i] << " ";
     }
     fout << endl;
     for (int i = 0; i < n1; i++) {
-        fout << ar_args[i] << " ";
+        fout << x_h[i] << " ";
     }
     fout << endl;
     for (int i = 0; i < n1; i++) {
-        fout << ar_vals[i] << " ";
+        fout << runge[i] << " ";
     }
     fout << endl;
     fout.close();
 }
 
 int main() {
-    function<double(double)> y = func;
-    function<double(double)> d_y = d_func;
 
-    double a = 0;
-    double b = 5;
+    double a = -3.14;
+    double b = 3.14;
 
-    int m_viz = 2000;
+    int m_v = 2000;
     int m1 = 11;
     int m2 = 2 * m1 - 1;
     double h1 = (b - a) / (m1 - 1);
     double h2 = (b - a) / (m2 - 1);
 
-    double args1[m1];
-    double vals1[m1];
-    double d_vals1[m1];
+    double x_h[m1];
+    double y_h[m1];
+    double dy_h[m1];
     double main_err[m1];
     double runge[m1];
     double der1[m1];
-    double args2[m2];
-    double vals2[m2];
-    double d_vals2[m2];
+    double x_h2[m2];
+    double y_h2[m2];
+    double dy_h2[m2];
     double der2[m2];
-    double viz_args[m_viz];
-    double viz_vals[m_viz];
+    double x_v[m_v];
+    double y_v[m_v];
 
 
-    f_even_args(viz_args, m_viz, a, b);
-    f_even_args(args1, m1, a, b);
-    f_even_args(args2, m2, a, b);
-    fill_func(vals1, y, args1, m1);
-    fill_func(vals2, y, args2, m2);
-    fill_func(viz_vals, d_y, viz_args, m_viz);
+    f_even(x_v, m_v, a, b);
+    f_even(x_h, m1, a, b);
+    f_even(x_h2, m2, a, b);
+    fill_func(y_h, x_h, m1);
+    fill_func(y_h2, x_h2, m2);
+    fill_dfunc(y_v, x_v, m_v);
 
-    fill_func(d_vals1, d_y, args1, m1);
-    fill_func(d_vals2, d_y, args2, m2);
+    fill_dfunc(dy_h, x_h, m1);
+    fill_dfunc(dy_h2, x_h2, m2);
 
-    differentiate(vals1, der1, m1, h1);
-    differentiate(vals2, der2, m2, h2);
+    differentiate(y_h, der1, m1, h1);
+    differentiate(y_h2, der2, m2, h2);
     calc_main_err(main_err, der1, der2, m1);
     calc_runge(runge, main_err, der1, m1);
 
-    write_errs1(d_vals1, der1, m1, d_vals2, der2, m2, runge,main_err, "errs1");
+    write_errs1(dy_h, der1, m1, dy_h2, der2, m2, runge, main_err, "errs1");
 
-    write_f(viz_args, viz_vals, args1, der1, args2, der2, args1, runge, m1, m2, m_viz);
+    write_f(x_v, y_v, x_h, der1, x_h2, der2, runge, m1, m2, m_v);
 
-    system("python3 repr.py");
-
+    system("python3 vis.py");
 
     return 0;
 }
